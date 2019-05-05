d87b663cdae44fb8d79827a149bfd9285c39271a
Lindsey Wells <lindsey.wells84@gmail.com>
2019-05-05 20:16:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 katz_deli = []
+queue = []
 
 def names_and_positions(katz_deli)
-  queue = []
   i=0
   katz_deli.each do |name|
     i+=1
@@ -9,13 +9,14 @@ def names_and_positions(katz_deli)
   end
   return queue
 end
+
   
 
 def line(katz_deli, queue)
   if katz_deli.length == 0
 puts "The line is currently empty."  
 else 
-  puts "The line is currently #{names_and_positions(katz_deli}"
+  puts "The line is currently #{queue}"
   
 end
 end
